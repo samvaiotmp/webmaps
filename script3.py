51d97c2d58c8aeb13fc78d20848cbcd590819455
samvaio <samvaiotmp@gmail.com>
2017-05-14 00:15:43 -0500

    home desktop

---
+++ script3.py
@@ -0,0 +1,26 @@
+import folium
+import pandas
+
+
+df=pandas.read_csv("Volcanoes-USA.txt")
+
+map = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=6, tiles='Stamen Terrain')
+
+def color(elev):
+    minimuum=int(min(df['ELEV']))
+    step=int((max(df['ELEV'])-min(df['ELEV']))/3)
+
+    if elev in range(minimuum,minimuum+step):
+        col='green'
+    elif elev in range(minimuum+step,minimuum+step*2):
+        col='orange'
+    else:
+        col='red'
+    return col
+
+for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
+    map.add_child(folium.Marker(location=[lat,lon],popup=name,icon=folium.Icon(color(elev),icon_color='grey')))
+
+map.add_child(folium.LayerControl())
+
+map.save(outfile='test1.html')
